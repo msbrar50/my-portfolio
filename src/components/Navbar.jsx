f59263e28aaaf7bb7a11315eff131d3a59f9fd42
MANPREET SINGH <knick.mapreet@gmail.com>
2025-04-03 23:11:58 +0100

    bootstrap added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,36 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="p-4 bg-gray-800 text-white flex justify-around">
-      <NavLink to="/" className="px-4" activeClassName="font-bold">Home</NavLink>
-      <NavLink to="/about" className="px-4" activeClassName="font-bold">About</NavLink>
-      <NavLink to="/projects" className="px-4" activeClassName="font-bold">Projects</NavLink>
-      <NavLink to="/skills" className="px-4" activeClassName="font-bold">Skills</NavLink>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/projects" className="nav-link" activeClassName="active">Projects</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/skills" className="nav-link" activeClassName="active">Skills</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
